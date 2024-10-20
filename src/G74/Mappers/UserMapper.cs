9581ca855a1902777cf918973b4913340df3f730
Rui Beloto <1121200@isep.ipp.pt>
2024-10-20 04:06:35 +0100

    Added implementation, AppServiceUser.cs, DBDriver.cs e mapper. #1

--- src/G74/Mappers/UserMapper.cs
+++ src/G74/Mappers/UserMapper.cs
@@ -1,6 +1,14 @@
+using G74.Domain.Aggregates.User;
+using G74.Domain.Value_Objects;
+using G74.Domain.Value_Objects.User;
+using G74.DTO;
+
 namespace G74.Mappers;
 
 public class UserMapper
 {
-    
+    public static User Create(VoUser voUser)
+    {
+        return new User(voUser.Username, voUser.Role, voUser.Email);
+    }
 }
--- src/G74/Startup.cs
+++ src/G74/Startup.cs
@@ -1,5 +1,13 @@
 ﻿using System.Text.Json;
+using G74.Adapters.Controllers;
+using G74.Adapters.Repositories;
+using G74.Domain.IRepositories;
+using G74.DTO;
+using G74.Infrastructure.Persistence;
+using G74.Infrastructure.Routes;
 using G74.Infrastructure.Shared;
+using G74.Mappers;
+using G74.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
@@ -21,7 +29,7 @@ public class Startup
             opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 .ReplaceService<IValueConverterSelector, StronglyEntityIdValueConverterSelector>()
                 .UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole())));
-
+        
         ConfigureMyServices(serviceCollection);
 
         serviceCollection.AddControllers().AddJsonOptions(options =>
@@ -55,5 +63,14 @@ public class Startup
 
     public void ConfigureMyServices(IServiceCollection services)
     {
+
+        services.AddTransient<JsonToDTO>();
+        services.AddScoped<RouteUser>();
+        services.AddScoped<ControllerUser>();
+        services.AddScoped<IRepoUser, RepoUser>();
+        services.AddScoped<AppServiceUser>();
+        services.AddTransient<UserMapper>();
+        services.AddTransient<ModelToData>();
+        services.AddScoped<IDBDriver, DBDriver>();
     }
 }
