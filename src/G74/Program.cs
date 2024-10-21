03fd9884fdefc9272e0bd37af58de877626e7416
Sergio <1210891@isep.ipp.pt>
2024-10-21 18:22:40 +0100

    Corrected problem with https redirection, program can be run now

--- src/G74/Program.cs
+++ src/G74/Program.cs
@@ -1,10 +1,4 @@
 using Microsoft.AspNetCore;
-using Microsoft.EntityFrameworkCore;
-using G74.Adapters.Repositories;
-using G74.Domain.IRepositories;
-using G74.Infrastructure.Persistence;
-using G74.Services;
-using G74.Mappers;
 
 namespace G74
 {
