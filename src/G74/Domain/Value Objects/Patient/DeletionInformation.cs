236c8879f1dceaeb25129fc3f45be99b2791d2bd
Sergio <1210891@isep.ipp.pt>
2024-10-25 22:01:51 +0100

    #10 Completed previous commit

---
+++ src/G74/Domain/Value Objects/Patient/DeletionInformation.cs
@@ -0,0 +1,24 @@
+﻿using G74.Domain.Shared;
+
+namespace G74.Domain.Value_Objects.Patient;
+
+public class DeletionInformation : IValueObject
+{
+    
+    public bool ToDelete { get;  }
+    
+    public DateTime ? DateToBeDeleted { get;  }
+    
+    protected DeletionInformation(){}
+
+    public DeletionInformation(bool toDelete, TimeSpan timeToDeletion)
+    {
+
+        ToDelete = toDelete;
+        if (ToDelete)
+        {
+            DateToBeDeleted = DateTime.Now.Add(timeToDeletion);
+        }
+
+    }
+}
