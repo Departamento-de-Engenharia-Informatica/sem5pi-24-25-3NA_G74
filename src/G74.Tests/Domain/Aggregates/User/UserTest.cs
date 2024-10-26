a9b3af00446a36b3fd8debcc10551f5225425c64
Rui Beloto <1121200@isep.ipp.pt>
2024-10-26 11:47:05 +0100

    Added test project for automatic testing. #1

---
+++ src/G74.Tests/Domain/Aggregates/User/UserTest.cs
@@ -0,0 +1,16 @@
+using G74.Domain.Aggregates.User;
+using NUnit.Framework;
+
+namespace G74.Tests.Domain.Aggregates.User;
+
+[TestFixture]
+[TestOf(typeof(G74.Domain.Aggregates.User.User))]
+public class UserTest
+{
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
+}
