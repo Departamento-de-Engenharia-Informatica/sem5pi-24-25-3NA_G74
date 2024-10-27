3ba57c9ecd6b021e7f9a1471cbe6d19caf411a23
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-10-27 17:13:45 +0000

    Unit tests for domain and service layer #12 #14

---
+++ src/G74.Tests/Domain/Value Objects/Staff/EmailTest.cs
@@ -0,0 +1,16 @@
+using G74.Domain.Value_Objects.Staff;
+using JetBrains.Annotations;
+using Xunit;
+
+namespace G74.Tests.Domain.Value_Objects.Staff;
+
+[TestSubject(typeof(Email))]
+public class EmailTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
