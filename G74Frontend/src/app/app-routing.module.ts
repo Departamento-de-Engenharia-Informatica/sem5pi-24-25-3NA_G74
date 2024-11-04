043dcaafe114f8f14581dda7d2e4112699c4d1fb
Sergio <1210891@isep.ipp.pt>
2024-11-04 21:30:11 +0000

    Created a simple frontend admin menu

--- G74Frontend/src/app/app-routing.module.ts
+++ G74Frontend/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { PatientCreateComponent } from './presentation/components/patient-create/patient-create.component';
 import { PatientUpdateComponent } from './presentation/components/patient-update/patient-update.component';
 import { PatientDeleteComponent } from './presentation/components/patient-delete/patient-delete.component';
+import { AdminMenuComponent } from './presentation/components/admin-menu/admin-menu.component';
 
 //Para os futuros a ver isso, não existe uma home ainda por isso o default por agora é redirecionar para o create patient (path: '') 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/create-patient', pathMatch: 'full' },  // Redirect root path to 'create-patient' for testing
-  { path: 'create-patient', component: PatientCreateComponent },
-  { path: 'update-patient', component: PatientUpdateComponent },
-  { path: 'delete-patient', component: PatientDeleteComponent}
+  { path: '', redirectTo: '/admin', pathMatch: 'full' },  // Redirect root to the admin menu
+  { path: 'admin', component: AdminMenuComponent },       // Admin menu standalone route
+  { path: 'admin/create-patient', component: PatientCreateComponent },
+  { path: 'admin/update-patient', component: PatientUpdateComponent },
+  { path: 'admin/delete-patient', component: PatientDeleteComponent }
 ];
 
 @NgModule({
--- G74Frontend/src/app/app.module.ts
+++ G74Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { AdminMenuComponent } from './presentation/components/admin-menu/admin-menu.component';
 import { PatientCreateComponent } from './presentation/components/patient-create/patient-create.component';
 import { PatientUpdateComponent } from './presentation/components/patient-update/patient-update.component';
 import { PatientDeleteComponent } from './presentation/components/patient-delete/patient-delete.component';
@@ -11,6 +12,7 @@ import { FormsModule } from '@angular/forms';  // Import FormsModule
 @NgModule({
   declarations: [
     AppComponent,
+    AdminMenuComponent,
     PatientCreateComponent,  // Add the component here
     PatientUpdateComponent,
     PatientDeleteComponent
