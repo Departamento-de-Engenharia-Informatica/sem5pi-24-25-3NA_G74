9ebd8392695d07814f90aaae1db9789694307ac6
Rui Beloto <1121200@isep.ipp.pt>
2024-11-06 23:23:06 +0000

    Added frontend pages and funcionality for login by google and connection with register. #34

--- G74Frontend/src/app/app.component.ts
+++ G74Frontend/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'G74Frontend';
+  ngOnInit(): void {
+    this.loadGoogleScript();
+  }
+
+  loadGoogleScript() {
+    const existingScript = document.getElementById('google-script');
+    if (!existingScript) {
+      const script = document.createElement('script');
+      script.id = 'google-script';
+      script.src = 'https://accounts.google.com/gsi/client';
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+    }
+  }
 }
---
+++ G74Frontend/src/app/presentation/components/main-menu/main-menu.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {
+
+  constructor(private router: Router) {}
+
+  redirectToRegister() {
+    this.router.navigate(['/register-user']);
+  }
+
+
+}
