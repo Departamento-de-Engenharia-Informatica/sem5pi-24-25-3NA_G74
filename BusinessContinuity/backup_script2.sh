2b498e21ca0032ea446ee21e8662b7facaaef385
pedroaguia8 <24630163+pedroaguia8@users.noreply.github.com>
2024-11-22 09:31:11 +0000

    Add verbose setting to tar #57

--- BusinessContinuity/backup_script2.sh
+++ BusinessContinuity/backup_script2.sh
@@ -12,18 +12,18 @@ mkdir -p "$BACKUP_DIR"
 
 # Connect to remote server and create full system backup
 # Excluding temporary and virtual filesystems
-ssh -p "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST" "tar --exclude=/proc \
-                                                      --exclude=/sys \
-                                                      --exclude=/dev \
-                                                      --exclude=/run \
-                                                      --exclude=/media \
-                                                      --exclude=/mnt \
-                                                      --exclude=/tmp \
-                                                      --exclude=lost+found \
-                                                      --exclude=/backup \
-                                                      --exclude=/var/cache \
-                                                      --exclude=/var/tmp \
-                                                      -czf - /" > "$BACKUP_DIR/fullsystem_$TIMESTAMP.tar.gz"
+ssh -p "$REMOTE_PORT" "$REMOTE_USER@$REMOTE_HOST" "tar -v --exclude=/proc \
+                                                          --exclude=/sys \
+                                                          --exclude=/dev \
+                                                          --exclude=/run \
+                                                          --exclude=/media \
+                                                          --exclude=/mnt \
+                                                          --exclude=/tmp \
+                                                          --exclude=lost+found \
+                                                          --exclude=/backup \
+                                                          --exclude=/var/cache \
+                                                          --exclude=/var/tmp \
+                                                          -czf - /" > "$BACKUP_DIR/fullsystem_$TIMESTAMP.tar.gz"
 
 # Keep only last 5 backups
 ls -t "$BACKUP_DIR"/fullsystem_*.tar.gz | tail -n +6 | xargs -r rm
