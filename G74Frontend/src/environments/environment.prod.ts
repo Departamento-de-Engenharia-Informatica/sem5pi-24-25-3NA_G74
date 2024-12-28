8e82f7179260e58ee948cf3d686400f327f37de7
Sergio <1210891@isep.ipp.pt>
2024-12-28 21:39:09 +0000

    Slight change to deploy script to enable the frontend

--- G74Frontend/src/environments/environment.prod.ts
+++ G74Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://vs-gate.dei.isep.ipp.pt:10568/api',  
+    apiUrl: 'http://vsgate-http.dei.isep.ipp.pt:10568/api',
     apiUrlNode: 'https://vs568.dei.isep.ipp.pt/nodeapi'
-  };
+};
