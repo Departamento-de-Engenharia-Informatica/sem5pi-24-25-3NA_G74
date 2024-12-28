23f4ca0c7fd8452765e3c896168fbca34e95f6c0
Sergio <1210891@isep.ipp.pt>
2024-12-28 01:12:59 +0000

    Attempt to improve the deployment

---
+++ G74Frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://vs-gate.dei.isep.ipp.pt:10568/api',  
+    apiUrlNode: 'https://vs568.dei.isep.ipp.pt/nodeapi'
+  };
--- src/G74/Services/PatientDeletionService.cs
+++ src/G74/Services/PatientDeletionService.cs
@@ -2,23 +2,31 @@ using System.Collections;
 using G74.DataModel;
 using G74.Domain;
 using G74.Domain.IRepositories;
+using Org.BouncyCastle.Utilities;
 
 namespace G74.Services;
 
 public class PatientDeletionService : BackgroundService
 {
     private readonly IServiceProvider _serviceProvider;
+    private readonly IConfiguration _configuration;
 
-    public PatientDeletionService(IServiceProvider serviceProvider)
+    public PatientDeletionService(IServiceProvider serviceProvider, IConfiguration configuration)
     {
         _serviceProvider = serviceProvider;
+        _configuration = configuration;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        
+        string timeStr = _configuration["GPRD:RefreshDelete"] ?? "1";
+
+        int time = int.Parse(timeStr);
+        
         while (!stoppingToken.IsCancellationRequested)
         {
-            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+            await Task.Delay(TimeSpan.FromMinutes(time), stoppingToken);
             await DeletePatientsAsync(stoppingToken);
         }
     }
