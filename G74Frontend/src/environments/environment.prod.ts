b464bc22c74bc7a1bd94acaaa4e2172fa1b1112b
Sergio <1210891@isep.ipp.pt>
2024-12-28 22:46:05 +0000

    Changed to relative path on the environment.prod.ts

--- G74Frontend/src/environments/environment.prod.ts
+++ G74Frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://vsgate-http.dei.isep.ipp.pt:10568/api',
+    apiUrl: '/api/',
     apiUrlNode: 'https://vs568.dei.isep.ipp.pt/nodeapi'
 };
